e26e7c54ab669ad704523294b62055e097646bcf
jrasmusson <rasmus4200@gmail.com>
2020-03-21 06:24:28 -0600

    Broke property out into own solution

---
+++ 7-properties/Playgrounds/3a-PropertyArcade.playground/Contents.swift
@@ -0,0 +1,72 @@
+/*
+  ___                       _            _                  _
+ | _ \_ _ ___ _ __  ___ _ _| |_ _  _    /_\  _ _ __ __ _ __| |___
+ |  _/ '_/ _ \ '_ \/ -_) '_|  _| || |  / _ \| '_/ _/ _` / _` / -_)
+ |_| |_| \___/ .__/\___|_|  \__|\_, | /_/ \_\_| \__\__,_\__,_\___|
+             |_|                |__/
+
+ */
+
+ import Foundation
+
+/*
+🕹 Dog years
+
+   Create a struct called person, with a storied property of type Int called age, and then add a computed property called ageInDogYears of type Int that returns the age of the person in dog years (age * 7).
+ */
+
+
+
+
+/*
+🕹 isEquilateral
+
+    Given the following Triangle stucture, write a computed property that determines whether the sides of the triangle are equilateral (all three sides are equal).
+
+    var isEquilateral: Bool {
+        return ?
+    }
+
+ */
+
+
+/*
+🕹 Title in the Post
+
+ Add a property observer stored property title that trims white space and new lines when the title stored property is set.
+
+ > struct Post
+ > var title: String
+ > title.trimmingCharacters(in: .whitespacesAndNewlines)
+
+ */
+
+
+
+/*
+🕹 Observe the radius
+
+    Define a class of type Circle. Give it a stored property `radius` of type `Double`.
+    And use the `didSet` property observer to ensure no negative values get assigned to radius.
+    If negative number, assign radius value of 0.
+
+ */
+
+
+
+/*
+🕹 Compute the area
+
+ Building on the previous example, calculate the circles area as a computed property.
+
+ Hint: area = radius * radius * Double.pi
+
+ */
+
+
+
+/*
+🕹 Set the radius via the square root.
+
+    Building on previous question, add a setter to the computed property area. When the area is set, have it set the stored property radius.
+ */
